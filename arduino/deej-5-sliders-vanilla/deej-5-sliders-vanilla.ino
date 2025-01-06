68b06f5e811a910df1472a606ea71b259798888f
Robin Carrupt <robincarrupt@gmail.com>
2025-01-06 20:21:36 +0100

    New code

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,100 +1,71 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
+
+#define LED_PIN    12
+#define LED_COUNT 5
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+#define NUM_SLIDERS 5
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const uint32_t sliderColors[NUM_SLIDERS] = 
+{
+  strip.Color(0x10, 0xF0, 0x20),
+  strip.Color(0xFC, 0x14, 0x12),
+  strip.Color(0x7F, 0x00, 0xFF),
+  strip.Color(0x00, 0x00, 0xFF),
+  strip.Color(0xFF, 0xBF, 0x00)
+};
 
 
-#define PIXEL_PIN    12
-#define PIXEL_COUNT  5
+int analogSliderValues[NUM_SLIDERS];
 
-Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-
-int brightnessValue[5];
-const int analogInputs[5] = {A0, A1, A2, A3, A4};
-const int digitalInputs[5] = {3, 4, 5, 6, 7};
-int analogSliderValues[5];
-bool digitalValue[5] = {1,1,1,1,1};
-bool digitalState[5];
-
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
-
-// Variables will change:
-int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-
-
-void setup() {
+void setup() { 
   strip.begin();
-  Serial.begin(9600);
-  for (int i = 0; i < 5; i++) {
+  strip.setBrightness(255);
+  for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
-    pinMode(digitalInputs[i], INPUT_PULLUP);
+    strip.setPixelColor(i,sliderColors[i]);
   }
+  Serial.begin(9600);
 }
 
 void loop() {
-
-
-  for (int i = 0; i < 5; i++)
-  {
-    if (digitalValue[i] == 1)
-    {
-      analogSliderValues[i] = analogRead(analogInputs[i]);
-      brightnessValue[i] = analogSliderValues[i] / 4.7  ;
-    }
-    if (digitalValue[i] == 0)
-    {
-      analogSliderValues[i] = 1024;
-      brightnessValue[i] = 255  ;
-    }
-  }
-
-  strip.setPixelColor(0, 0, 255 - brightnessValue[0], 0);
-  strip.setPixelColor(1, 255 - brightnessValue[1], 100 - (brightnessValue[1] / 2.55), 0);
-  strip.setPixelColor(2 , 0, 0, 255 - brightnessValue[2]);
-  strip.setPixelColor(3 , 100 - (brightnessValue[3] / 2.55), 0, 255 - brightnessValue[3]);
-  strip.setPixelColor(4 , 255 - brightnessValue[4], 0, 0);
+  updateSliderValues();
   strip.show();
-
-  sendSliderValues();
+  sendSliderValues(); // Actually send data (all the time)
+  // printSliderValues(); // For debug
   delay(10);
-  
-  for (int i = 0; i < 5; i++)
-  {
-    strip.show();
-    digitalState[i] = digitalRead(digitalInputs[i]);
-    if(digitalState[i] == 0)
-    {
-      delay(50);
-      strip.show();
-      if(digitalRead(digitalInputs[i]) == 0)
-      {
-        digitalValue[i] =! digitalValue[i];
-        while(digitalRead(digitalInputs[i]) == 0)
-        {
-          sendSliderValues();
-          strip.show();
-          delay(10);
-        }
-      }
-    }
+}
+
+void updateSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+     analogSliderValues[i] = analogRead(analogInputs[i]);
   }
 }
 
 void sendSliderValues() {
   String builtString = String("");
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
     builtString += String((int)analogSliderValues[i]);
 
-    if (i < 5 - 1) {
+    if (i < NUM_SLIDERS - 1) {
       builtString += String("|");
     }
   }
-
+  
   Serial.println(builtString);
+}
+
+void printSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_SLIDERS - 1) {
+      Serial.write(" | ");
+    } else {
+      Serial.write("\n");
+    }
+  }
 }
